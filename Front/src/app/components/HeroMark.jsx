3f05e7d84beaba68ff124ef0e3dbb3ef93770f8b
MigVarona <migvaronag@gmail.com>
2024-02-21 18:43:28 +0100

    ok

--- Front/src/app/components/HeroMark.jsx
+++ Front/src/app/components/HeroMark.jsx
@@ -13,7 +13,7 @@ export default function HeroMark() {
               <div className="flex h-full items-center justify-center">
                 <div className="px-6 text-center text-white md:px-12">
                   <h1 className="mt-6 mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
-                    The best offer on the market <br /><span>for your business</span>
+                  El Marketplace  Nº1 en España <br /><span></span>
                   </h1>
                   <a className="mb-2 inline-block rounded-full border-2 border-neutral-50 px-[46px] pt-[14px] pb-[12px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 md:mr-2 md:mb-0"
                     data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Get started</a>
@@ -31,12 +31,12 @@ export default function HeroMark() {
                 fill="currentColor"></path>
             </svg>
           </div>
-<div className="mt-20 gap-10 flex items-center justify-center">
+
+          <div className="mt-20 font-bold gap-10 flex items-center justify-center">
           <a className="link link-hover">DISPOSITIVOS</a>
           <a className="link link-hover">ROBOTS</a>
           <a className="link link-hover">IA</a>
           </div>
-       
         </section>
        
     )
--- Front/src/app/marketplace/page.jsx
+++ Front/src/app/marketplace/page.jsx
@@ -1,6 +1,9 @@
 
 import HeroMark from "../components/HeroMark"
 import Card from "../components/Card"
+import Divider from "../components/Divider"
+import Footer from "../components/Footer";
+import Gallery from "../components/Gallery";
 
 
 function Productos() {
@@ -8,10 +11,15 @@ function Productos() {
 
         <div>
           <HeroMark />
-          <div className="flex gap-8 justify-center items-center">
+          <div className="flex flex-col  justify-center items-center">
           <Card />
+       
           
+        
           </div>
+          <Divider />
+          <Gallery />
+          <Footer />
         </div>
      );
 }
