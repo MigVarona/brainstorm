bb92e60f42c00b00cbf395d51523d3c41ac0a077
MigVarona <migvaronag@gmail.com>
2024-02-21 17:47:46 +0100

    CARDS

--- Front/src/app/layout.jsx
+++ Front/src/app/layout.jsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
--- Front/src/app/marketplace/page.jsx
+++ Front/src/app/marketplace/page.jsx
@@ -1,13 +1,17 @@
 
-
+import HeroMark from "../components/HeroMark"
+import Card from "../components/Card"
 
 
 function Productos() {
     return ( 
 
         <div>
+          <HeroMark />
+          <div className="flex gap-8 justify-center items-center">
+          <Card />
           
-          
+          </div>
         </div>
      );
 }
