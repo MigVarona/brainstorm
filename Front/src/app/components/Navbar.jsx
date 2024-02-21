b67ade433c93ccd4a778f730716b59232fd58ace
MigVarona <migvaronag@gmail.com>
2024-02-21 23:20:25 +0100

    Contact & Links

--- Front/src/app/components/Navbar.jsx
+++ Front/src/app/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Home', href: '/' },
+  { name: 'Seller', href: 'clientes' },
+  { name: 'Marketplace', href: 'marketplace' },
+  { name: 'Contact', href: '#' },
 ]
 
 export default function Navbar() {
@@ -46,7 +46,7 @@ export default function Navbar() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="login" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -87,7 +87,7 @@ export default function Navbar() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
---
+++ Front/src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import Login from "../components/Login"
+import Footer from "../components/Footer"
+
+function Register() {
+    return ( 
+
+        <div>
+          <Login />
+          <Footer />
+        </div>
+     );
+}
+
+export default Register;
