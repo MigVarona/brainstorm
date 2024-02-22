5b40c6dfea726c67614be00847f28036287913c3
San-QP <s.rebollar.g@gmail.com>
2024-02-22 10:08:19 +0100

    add productsController, Model and Router

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
-import buyersRouter from './Backend/routes/buyersRouter.js';
-import sellersRouter from './Backend/routes/sellersRouter.js';
+import productsRouter from './Backend/routes/productsRouter.js';
+// import sellersRouter from './Backend/routes/sellersRouter.js';
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 /* app.use("/", ); */
-app.use("/marketplace", buyersRouter);
-app.use("/newproduct", sellersRouter);
+app.use("/products", productsRouter);
+// app.use("/newproduct", sellersRouter);
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
