03db313f7ef626c2282a72a9879134b8df3c9313
MigVarona <migvaronag@gmail.com>
2024-02-23 18:24:40 +0100

    ok

--- Front/src/app/components/Hero.test.js
+++ Front/src/app/components/Hero.test.js
@@ -6,11 +6,9 @@ import Hero from './Hero';
 test('renders correct content and links', () => {
     const { getByText, getByTestId } = render(<Hero />);
   
-    // Verifica el título y el texto
     expect(getByText('BrainStorm')).toBeInTheDocument();
     expect(getByText('Descubre un mundo de posibilidades en nuestro marketplace líder en inteligencia artificial y robots diseñados para adaptarse a tus necesidades.')).toBeInTheDocument();
   
-    // Verifica la presencia de enlaces
     expect(getByTestId('marketplace-link')).toHaveAttribute('href', 'marketplace');
     expect(getByTestId('seller-link')).toHaveAttribute('href', 'clientes');
   });
